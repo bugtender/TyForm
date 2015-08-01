a12827cb207168d08225a1ec438ac3f57abb4f8c
Vincent Lin <viflin@gmail.com>
2015-08-01 14:18:59 +0800

    fix jQuery UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui
+//= require jquery-ui/sortable
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
